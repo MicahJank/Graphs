29df1ef259d0010926f09e5c2360268239476957
MicahJank <MicahJank@users.noreply.github.com>
2020-07-30 13:33:21 -0500

    making some progress on the sprint...i think....

--- projects/adventure/adv.py
+++ projects/adventure/adv.py
@@ -5,16 +5,18 @@ from world import World
 import random
 from ast import literal_eval
 
+from util import Queue, Stack
+
 # Load world
 world = World()
 
 
 # You may uncomment the smaller graphs for development and testing purposes.
-# map_file = "maps/test_line.txt"
+map_file = "maps/test_line.txt"
 # map_file = "maps/test_cross.txt"
 # map_file = "maps/test_loop.txt"
 # map_file = "maps/test_loop_fork.txt"
-map_file = "maps/main_maze.txt"
+# map_file = "maps/main_maze.txt"
 
 # Loads the map into a dictionary
 room_graph=literal_eval(open(map_file, "r").read())
@@ -25,11 +27,61 @@ world.print_rooms()
 
 player = Player(world.starting_room)
 
-# Fill this out with directions to walk
+
+# --------MY STUFF STARTS HERE------------- #
+
+class Graph:
+    def __init__(self):
+        self.rooms = {}
+    
+    # nodes
+    def add_room(self, room_id):
+        self.rooms[room_id] = {}
+    
+    # edges
+    def add_exit(self, current_room, exit):
+        self.rooms[current_room][exit] = '?'
+
+    # finds the exits for whatever room id is entered
+    def get_room_exits(self, room_id):
+        return self.rooms[room_id]
+
+    def get_random_direction(self, room_id):
+        exits_list = list(self.rooms[room_id])
+
+        # should give back directions to rooms that havent been explored
+        choices = [direction for direction in exits_list if self.rooms[room_id][direction] == "?"]
+        # choose a random direction from the list
+        random_direction = random.choice(choices)
+
+        return random_direction
+
+
 # traversal_path = ['n', 'n']
+# traversal_path is what the test code will use to walk through all the rooms
 traversal_path = []
 
 
+rooms_graph = Graph()
+# add starting room to graph
+current_room = player.current_room
+rooms_graph.add_room(current_room.id)
+
+# add the starting rooms exits
+exits = player.current_room.get_exits()
+for direction in exits:
+    rooms_graph.add_exit(current_room.id, direction)
+
+random_direction = random.choice(exits)
+
+# room_stack = Stack()
+while rooms_graph[current_room.id][random_direction] !== "?":
+
+
+
+
+
+# --------MY STUFF ENDS HERE------------- #
 
 # TRAVERSAL TEST
 visited_rooms = set()
