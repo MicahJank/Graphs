9cee5badcd6467afcd7aeda8d9f5814377ee0473
MicahJank <MicahJank@users.noreply.github.com>
2020-07-31 12:36:32 -0500

    got the smallest map to pass - need to work on adding a bfs to find the nearest ? room

--- projects/adventure/adv.py
+++ projects/adventure/adv.py
@@ -12,8 +12,8 @@ world = World()
 
 
 # You may uncomment the smaller graphs for development and testing purposes.
-map_file = "maps/test_line.txt"
-# map_file = "maps/test_cross.txt"
+# map_file = "maps/test_line.txt"
+map_file = "maps/test_cross.txt"
 # map_file = "maps/test_loop.txt"
 # map_file = "maps/test_loop_fork.txt"
 # map_file = "maps/main_maze.txt"
@@ -46,15 +46,33 @@ class Graph:
     def get_room_exits(self, room_id):
         return self.rooms[room_id]
 
+    # useful for when i need to fill in the graph with the previous room information
+    # i.e if i go to a north room then my current room needs to know the previous
+    # room is to the south
+    def get_opposite_direction(self, direction):
+        if direction == "n":
+            return "s"
+        elif direction == "s":
+            return "n"
+        elif direction == "w":
+            return "e"
+        elif direction == "e":
+            return "w"
+
     def get_random_direction(self, room_id):
         exits_list = list(self.rooms[room_id])
 
         # should give back directions to rooms that havent been explored
         choices = [direction for direction in exits_list if self.rooms[room_id][direction] == "?"]
-        # choose a random direction from the list
-        random_direction = random.choice(choices)
 
-        return random_direction
+        # if the player tries to get a random direction in a room where there are no
+        # unexplored paths - then i should return None
+        if len(choices) > 0:
+            # choose a random direction from the list
+            random_direction = random.choice(choices)
+            return random_direction
+        else:
+            return None
 
 
 # traversal_path = ['n', 'n']
@@ -63,6 +81,8 @@ traversal_path = []
 
 
 rooms_graph = Graph()
+# keep track of the previous room so that i can update its rooms as i move onto the next
+prev_room = None
 # add starting room to graph
 current_room = player.current_room
 rooms_graph.add_room(current_room.id)
@@ -72,11 +92,37 @@ exits = player.current_room.get_exits()
 for direction in exits:
     rooms_graph.add_exit(current_room.id, direction)
 
-random_direction = random.choice(exits)
+random_direction = rooms_graph.get_random_direction(current_room.id)
 
-# room_stack = Stack()
-while rooms_graph[current_room.id][random_direction] !== "?":
 
+while random_direction is not None:
+    prev_room = current_room
+    # add the direction we are travelling into our traversal path
+    traversal_path.append(random_direction)
+    # travel in the random direction
+    player.travel(random_direction)
+    # re-assign the current room to the room we travelled to
+    current_room = player.current_room
+    # fill in previous room with the updated direction since i now have that
+    # rooms info
+    rooms_graph.rooms[prev_room.id][random_direction] = current_room.id
+
+
+    # add the new room to the graph
+    rooms_graph.add_room(current_room.id)
+
+    # get all of the current rooms exits and fill in the graph
+    exits = player.current_room.get_exits()
+    for direction in exits:
+        rooms_graph.add_exit(current_room.id, direction)
+
+    # i also need to update the current room with the prev room information
+    # get the opposite direction to fill in the prev room information for the current room
+    opposite_direction = rooms_graph.get_opposite_direction(random_direction)
+    rooms_graph.rooms[current_room.id][opposite_direction] = prev_room.id
+
+    # update random direction to continue the loop until there are no explored rooms
+    random_direction = rooms_graph.get_random_direction(current_room.id)
 
 
 
@@ -103,12 +149,12 @@ else:
 #######
 # UNCOMMENT TO WALK AROUND
 #######
-player.current_room.print_room_description(player)
-while True:
-    cmds = input("-> ").lower().split(" ")
-    if cmds[0] in ["n", "s", "e", "w"]:
-        player.travel(cmds[0], True)
-    elif cmds[0] == "q":
-        break
-    else:
-        print("I did not understand that command.")
+# player.current_room.print_room_description(player)
+# while True:
+#     cmds = input("-> ").lower().split(" ")
+#     if cmds[0] in ["n", "s", "e", "w"]:
+#         player.travel(cmds[0], True)
+#     elif cmds[0] == "q":
+#         break
+#     else:
+#         print("I did not understand that command.")
